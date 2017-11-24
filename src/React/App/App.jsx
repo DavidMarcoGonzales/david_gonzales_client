b7ae409f06d00e6ec57716faedae5c6ab4894d7e
David M. Gonzales <david_m_gonzales@hotmail.com>
2017-11-24 02:11:48 -0700

    added tag comments

--- src/React/App/App.jsx
+++ src/React/App/App.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
 import Workbook from "../Workbook/Workbook.jsx";
-// import CoursesPage from "../React-Redux-Test/CoursesPage";
+// Test Test Test (Start) Test Test Test
 import CoursesPage from "../../React-Redux/React-Redux-Test/CoursesPage";
-import {  NavLink, Route } from "react-router-dom";
+// Test Test Test ( End ) Test Test Test
+
+import { NavLink, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -11,14 +13,18 @@ class App extends Component {
       <div className="container">
         <div className="navbar navbar-default" style={{ marginBottom: '0px' }} >
           <ul className="nav navbar-nav">
-          <li className="nav"><NavLink to="/api/Workbook">TCI Student Workbook</NavLink></li>
-          <li className="nav"><NavLink to="/api/Test">Test</NavLink></li>
+            <li className="nav"><NavLink to="/api/Workbook">TCI Student Workbook</NavLink></li>
+            {/* Test Test Test (Start) Test Test Test */}
+            <li className="nav"><NavLink to="/api/Test">Test</NavLink></li>
+            {/* Test Test Test ( End ) Test Test Test */}
+
           </ul>
         </div>
         <Route path="/api/Workbook" component={Workbook} />
+        {/* Test Test Test (Start) Test Test Test */}
         <Route path="/api/Test" component={CoursesPage}  />
-
-        </div>
+        {/* Test Test Test ( End ) Test Test Test */}
+      </div>
     );
   }
 }
--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -2,11 +2,17 @@
  * Created by David on 11/8/2016.
  */
 import {combineReducers} from "redux";
-import courses from "../../React-Redux/React-Redux-Test/courseReducer";
 import card from "../../React-Redux/Card/cardReducer";
 
+// Test Test Test (Start) Test Test Test
+import courses from "../../React-Redux/React-Redux-Test/courseReducer";
+// Test Test Test ( End ) Test Test Test
+
 const rootReducer = combineReducers({
+  // Test Test Test (Start) Test Test Test
   courses: courses,
+  // Test Test Test ( End ) Test Test Test
+
   card: card
 });
 
