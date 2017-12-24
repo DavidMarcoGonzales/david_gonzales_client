8c7affe50297448631be683da4c452519c219701
David M. Gonzales <david_m_gonzales@hotmail.com>
2017-12-24 05:16:11 -0700

    changes to vidcard cardcc cardactions

--- src/React-Redux/Card/Redux/actions/cardActions.js
+++ src/React-Redux/Card/Redux/actions/cardActions.js
@@ -1,13 +1,24 @@
-import { SET_CURRENT_CARD, SET_CARD_SUCCESS } from "./actionTypes";
+import {
+  SET_CURRENT_CARD,
+  SET_CARD_SUCCESS
+} from "./actionTypes";
 import axios from 'axios';
 
-import { development } from "../../../../config";
+import {
+  development
+} from "../../../../config";
 
 export function setCard(card) {
-  return ({ type: SET_CURRENT_CARD, card: card });
+  return ({
+    type: SET_CURRENT_CARD,
+    card: card
+  });
 }
 export function setCardSuccess(card) {
-  return ({ type: SET_CARD_SUCCESS, card: card });
+  return ({
+    type: SET_CARD_SUCCESS,
+    card: card
+  });
 }
 
 export function asyncSetCard(uri) {
@@ -15,8 +26,21 @@ export function asyncSetCard(uri) {
     if (development) {
       uri = `https://david-gonzales-1.herokuapp.com${uri}`
     }
-    axios.get(uri)  // when testing local server 3000 and when deploying no cors
-    .then(response => {
+    axios.get(uri) // when testing local server 3000 and when deploying no cors
+      .then(response => {
+        dispatch(setCardSuccess(response.data[0]));
+      })
+      .catch(error => {
+        throw (error);
+      });
+  };
+}
+
+export function asyncSetCard2(uri) {
+  return (dispatch) => {
+    uri = `https://behavior-support.herokuapp.com/cards/${uri}`
+    axios.get(uri) // when testing local server 3000 and when deploying no cors
+      .then(response => {
         dispatch(setCardSuccess(response.data[0]));
       })
       .catch(error => {
--- src/React-Redux/Card/VidCard.jsx
+++ src/React-Redux/Card/VidCard.jsx
@@ -1,55 +1,51 @@
-import React, {  Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { asyncSetCard } from './Redux/actions/cardActions';
+import { asyncSetCard2 } from './Redux/actions/cardActions';
 import { connect } from "react-redux";
 import CardPC from './CardPC';
 
 
 class Card extends Component {
 
-    componentWillMount() {
-      alert("vidcard")
-        let myURI = ''; {
-            let { api, page, section, subsection } = this.props.match.params;
-            myURI = `/${api}/${page}/${section}/${subsection}/1`;
-        }
-        this.props.dispatch(asyncSetCard(myURI));
-    }
-    componentWillReceiveProps(nextProps) {
-        let myURI = ''; {
-            let { api, page, section, subsection, card } = this.props.match.params;
+  componentWillMount() {
+    let { card } = this.props.match.params;
+    this.props.dispatch(asyncSetCard2(card));
+  }
+  componentWillReceiveProps(nextProps) {
+    let myURI = ''; {
+      let { api, page, section, subsection, card } = this.props.match.params;
 
-            if (card === undefined)
-            card=1;
-            myURI = `/${api}/${page}/${section}/${subsection}/${card}`;
-        }
-        let myNewURI = ''; {
-            let { api, page, section, subsection, card } = nextProps.match.params;
-            if (card === undefined)
-            card=1;
-            myNewURI = `/${api}/${page}/${section}/${subsection}/${card}`
-        }
-        if (myURI !== myNewURI) {
-            this.props.dispatch(asyncSetCard(myNewURI));
-        }
+      if (card === undefined)
+        card = 1;
+      myURI = `/${api}/${page}/${section}/${subsection}/${card}`;
     }
-    render() {
-        return (
-            <div className='col-md-9' style={{ border: '1px solid rgb(235,235,235)', marginBottom: '40px', paddingTop: '15px' }}>
-                <CardPC
-                    card={this.props.card}
-                    dispatch={this.props.dispatch}
-                />
-            </div>
-        );
+    let myNewURI = ''; {
+      let { api, page, section, subsection, card } = nextProps.match.params;
+      if (card === undefined)
+        card = 1;
+      myNewURI = `/${api}/${page}/${section}/${subsection}/${card}`
     }
+    if (myURI !== myNewURI) {
+      this.props.dispatch(asyncSetCard2(myNewURI));
+    }
+  }
+  render() {
+    return (
+      <div className='col-md-9' style={{ border: '1px solid rgb(235,235,235)', marginBottom: '40px', paddingTop: '15px' }}>
+        <CardPC
+          card={this.props.card}
+          dispatch={this.props.dispatch}
+        />
+      </div>
+    );
+  }
 }
 Card.propTypes = {
-    card: PropTypes.object.isRequired
+  card: PropTypes.object.isRequired
 }
 function mapStateToProps(state) {
-    return {
-        card: state.card
-    }
+  return {
+    card: state.card
+  }
 }
 export default connect(mapStateToProps)(Card);
